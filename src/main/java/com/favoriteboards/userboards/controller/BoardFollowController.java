c454da11d852e840fd75fdb80e67261d7db0e4a6
ejariasm <ejarisasm@unal.edu.co>
2020-04-26 19:15:30 -0500

    Updated Board

--- src/main/java/com/favoriteboards/userboards/controller/BoardFollowController.java
+++ src/main/java/com/favoriteboards/userboards/controller/BoardFollowController.java
@@ -35,17 +35,16 @@ public class BoardFollowController {
     }
 
 
-
     @GetMapping("/getAll2")
     public ResponseEntity<List<BoardFollowDTO>> getAllBoardsFollow2() {
 
         List<BoardFollowDTO> boardFollowDTOs = new ArrayList<BoardFollowDTO>();
 
-        List<BoardFollow> boards =  boardFollowService.findAll();
+        List<BoardFollow> boards = boardFollowService.findAll();
 
-        for(BoardFollow bw: boards){
-            boardFollowDTOs.add(new BoardFollowDTO(bw.getId(),bw.getCreatedAt(),
-                                                    bw.getBoard().getId(),bw.getUser().getId()));
+        for (BoardFollow bw : boards) {
+            boardFollowDTOs.add(new BoardFollowDTO(bw.getId(), bw.getCreatedAt(),
+                    bw.getBoard().getId(), bw.getUser().getId()));
         }
 
         return ResponseEntity.ok(boardFollowDTOs);
@@ -53,77 +52,80 @@ public class BoardFollowController {
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<BoardFollow> getBoardFollowById(@PathVariable(value = "id") Long id){
+    public ResponseEntity<BoardFollow> getBoardFollowById(@PathVariable(value = "id") Long id) {
         return ResponseEntity.ok(boardFollowService.getBoardFollow(id));
     }
 
     @GetMapping("/getAllBoardsFollowByUser/{user_id}")
-    public ResponseEntity<List<Board>> getAllBoardsFollowByUser(@PathVariable(value = "user_id") Long user_id){
+    public ResponseEntity<List<Board>> getAllBoardsFollowByUser(@PathVariable(value = "user_id") Long user_id) {
 
         return ResponseEntity.ok(boardFollowService.findByUserId(user_id));
     }
 
 
+    @PostMapping(path = "create/user/{userId}/board/{boardId}", consumes = "application/json")
+    public ResponseEntity<BoardFollow> createBoardFollow(@PathVariable(value = "userId") Long userId,
+                                                         @PathVariable(value = "boardId") Long boardId) {
+        try {
 
-    @PostMapping(path ="create/user/{userId}/board/{boardId}", consumes = "application/json")
-    public ResponseEntity<BoardFollow> createBoardFollow(@PathVariable (value = "userId") Long userId,
-                                            @PathVariable (value = "boardId") Long boardId) {
-        try{
-
-                BoardFollow boardFollow= new BoardFollow();
-                User user= userService.getUser(userId);
-                boardFollow.setUser(user);
-
-
-                Board board = boardService.getBoard(boardId);
-                boardFollow.setBoard(board);
-
-                boardFollowService.createBoardFollow(boardFollow);
-
-                return new ResponseEntity<>(boardFollow, HttpStatus.CREATED);
-
-        }catch (Exception e){
-            return new ResponseEntity<>(null,HttpStatus.FORBIDDEN);
-        }
-    }
-
-    @PostMapping(path ="create", consumes = "application/json")
-    public ResponseEntity<BoardFollow> createBoardFollow(@RequestBody BoardFollowDTO boardFollowDTO) {
-        try{
-
-            BoardFollow boardFollow= new BoardFollow();
-            User user= userService.getUser(boardFollowDTO.getUser_id());
+            BoardFollow boardFollow = new BoardFollow();
+            User user = userService.getUser(userId);
             boardFollow.setUser(user);
 
 
-            Board board = boardService.getBoard(boardFollowDTO.getBoard_id());
+            Board board = boardService.getBoard(boardId);
             boardFollow.setBoard(board);
 
             boardFollowService.createBoardFollow(boardFollow);
 
             return new ResponseEntity<>(boardFollow, HttpStatus.CREATED);
 
-        }catch (Exception e){
-            return new ResponseEntity<>(null,HttpStatus.FORBIDDEN);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @PostMapping(path = "create", consumes = "application/json")
+    public ResponseEntity<BoardFollow> createBoardFollow(@RequestBody BoardFollowDTO boardFollowDTO) {
+        try {
+
+            BoardFollow boardFollow = new BoardFollow();
+            User user = userService.getUser(boardFollowDTO.getUser_id());
+            Board board = boardService.getBoard(boardFollowDTO.getBoard_id());
+
+
+            if (user != null && board != null) {
+                boardFollow.setUser(user);
+                boardFollow.setBoard(board);
+                boardFollowService.createBoardFollow(boardFollow);
+                return new ResponseEntity<>(boardFollow, HttpStatus.CREATED);
+
+            } else {
+                return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+            }
+
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
         }
     }
 
 
-    @DeleteMapping(path ="/delete/{boardFollowId}")
-    public ResponseEntity<Long> deleteBoardFollow(@PathVariable (value = "boardFollowId") Long boardFollowId) {
-        try{
-            Long count=boardFollowService.countBoardFollow(boardFollowId);
-            if (count==1) {
+    @DeleteMapping(path = "/delete/{boardFollowId}")
+    public ResponseEntity<Long> deleteBoardFollow(@PathVariable(value = "boardFollowId") Long boardFollowId) {
+        try {
+            Long count = boardFollowService.countBoardFollow(boardFollowId);
+            if (count == 1) {
 
                 BoardFollow boardFollowDelete = boardFollowService.getBoardFollow(boardFollowId);
 
                 boardFollowService.deleteBoardFollow(boardFollowDelete);
                 return new ResponseEntity<>(count, HttpStatus.ACCEPTED);
-            }else {
-                return new ResponseEntity<>(count,HttpStatus.BAD_REQUEST);
+            } else {
+                return new ResponseEntity<>(count, HttpStatus.BAD_REQUEST);
             }
-        }catch (Exception e){
-            return new ResponseEntity<>((long) -1,HttpStatus.FORBIDDEN);
+        } catch (Exception e) {
+            return new ResponseEntity<>((long) -1, HttpStatus.FORBIDDEN);
         }
     }
 
